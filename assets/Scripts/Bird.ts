903fc64891b32f6b3d615a51fe7de9cdf767acd6
Đoàn Tiến Triển <triendoan4432@gmail.com>
2023-09-25 11:49:09 +0700

    Flappy Bird

---
+++ assets/Scripts/Bird.ts
@@ -0,0 +1,64 @@
+import {
+  _decorator,
+  CCFloat,
+  Component,
+  Node,
+  Vec3,
+  Animation,
+  tween,
+  easing,
+} from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("Bird")
+export class Bird extends Component {
+  @property({
+    type: CCFloat,
+    tooltip: "How high can the fly",
+  })
+  public jumHeight: number = 3.5;
+
+  @property({
+    type: CCFloat,
+    tooltip: "How high can the fly",
+  })
+  public jumDuration: number = 3.5;
+
+  public birdAnimation: Animation;
+  public birdLocation: Vec3;
+
+  public hitSomething: boolean;
+
+  onLoad() {
+    this.resetBird();
+    this.birdAnimation = this.getComponent(Animation);
+  }
+
+  resetBird() {
+    this.birdLocation = new Vec3(0, 0, 0);
+    this.node.setPosition(this.birdLocation);
+    this.hitSomething = false;
+  }
+
+  fly() {
+    this.birdAnimation.stop();
+    tween(this.node.position)
+      .to(
+        this.jumDuration,
+        new Vec3(
+          this.node.position.x,
+          this.node.position.y + this.jumHeight,
+          0
+        ),
+        {
+          easing: "smooth",
+          onUpdate: (target: Vec3, ratio: number) => {
+            this.node.position = target;
+          },
+        }
+      )
+
+      .start();
+    this.birdAnimation.play();
+  }
+}
---
+++ assets/Scripts/GameCtrl.ts
@@ -0,0 +1,156 @@
+import {
+  _decorator,
+  CCInteger,
+  Component,
+  director,
+  EventKeyboard,
+  input,
+  Input,
+  KeyCode,
+  Node,
+  Contact2DType,
+  Collider2D,
+  IPhysics2DContact,
+} from "cc";
+const { ccclass, property } = _decorator;
+import { Ground } from "./Ground";
+import { Results } from "./Results";
+import { Bird } from "./Bird";
+import { PipePool } from "./PipePool";
+import { BirdAudio } from "./BirdAudio";
+
+@ccclass("GameCtrl")
+export class GameCtrl extends Component {
+  @property({
+    type: Component,
+    tooltip: "This is ground",
+  })
+  public ground: Ground;
+
+  @property({
+    type: Results,
+    tooltip: "Results go here",
+  })
+  public results: Results;
+
+  @property({
+    type: Bird,
+  })
+  public bird: Bird;
+
+  @property({
+    type: PipePool,
+  })
+  public pipeQueue: PipePool;
+
+  @property({
+    type: BirdAudio,
+  })
+  public clip: BirdAudio;
+
+  @property({
+    type: CCInteger,
+  })
+  public speed: number = 300;
+
+  @property({
+    type: CCInteger,
+  })
+  public pipeSpeed: number = 200;
+
+  public isOver: boolean;
+
+  onLoad() {
+    this.initListener();
+    this.results.resestScore();
+    this.isOver = true;
+    director.pause();
+  }
+
+  initListener() {
+    // input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+
+    this.node.on(Node.EventType.TOUCH_START, () => {
+      if (this.isOver == true) {
+        this.resetGame();
+        this.bird.resetBird();
+        this.startGame();
+      }
+      if (this.isOver == false) {
+        this.bird.fly();
+        this.clip.onAudioQueue(0);
+      }
+    });
+  }
+
+  // onKeyDown(event: EventKeyboard) {
+  //   switch (event.keyCode) {
+  //     case KeyCode.KEY_A:
+  //       this.gameOver();
+  //       break;
+  //     case KeyCode.KEY_P:
+  //       this.results.addScore();
+  //       break;
+  //     case KeyCode.KEY_Q:
+  //       this.resetGame();
+  //       this.bird.resetBird();
+  //   }
+  // }
+
+  startGame() {
+    this.results.hideResults();
+    director.resume();
+  }
+
+  gameOver() {
+    this.results.showResults();
+    this.isOver = true;
+    this.clip.onAudioQueue(3);
+    director.pause();
+  }
+
+  resetGame() {
+    this.results.resestScore();
+    this.pipeQueue.reset();
+    this.isOver = false;
+    this.startGame();
+  }
+
+  passPipe() {
+    this.results.addScore();
+    this.clip.onAudioQueue(1);
+  }
+
+  createPipe() {
+    this.pipeQueue.addPool();
+  }
+
+  contactGroundPipe() {
+    let collider = this.bird.getComponent(Collider2D);
+
+    if (collider) {
+      collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+    }
+  }
+  onBeginContact(
+    selfCollider: Collider2D,
+    otherCollider: Collider2D,
+    contact: IPhysics2DContact | null
+  ) {
+    this.bird.hitSomething = true;
+    this.clip.onAudioQueue(2);
+  }
+
+  birdStruck() {
+    this.contactGroundPipe();
+    if (this.bird.hitSomething == true) {
+      this.gameOver();
+    }
+  }
+
+  update() {
+    if (this.isOver == false) {
+      this.birdStruck();
+    }
+  }
+}
